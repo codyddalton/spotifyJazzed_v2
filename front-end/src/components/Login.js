5af7cada0203dc419d4faef066eb8e63b3b207fe
Cody <cddalton@student.fullsail.edu>
2026-02-05 12:31:34 -0500

    More changes to build

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -28,7 +28,7 @@ export default function Login(props){
             <div id="Login-zone" style={{backgroundColor:"#43435fc9", width:"45%", alignItems:"center", display:"flex", flexDirection:"column", borderRadius:"25px", marginBottom:"4%"}}>
                 <p style={{fontSize:"1.75rem", fontFamily:"Courier New; monospace", fontWeight:"600", letterSpacing:"5px", color:"#42C6FF", marginTop:"15%", marginBottom:"10%"}}><i><b style={{color:"#F47DA8", fontSize:"2.5rem"}}>♪</b>Time to Get <b style={{color:"#467DE8", fontSize:"1.85rem"}}>Jazzy</b><b style={{color:"#F47DA8", fontSize:"2.5rem"}}>♫</b></i></p>
                 <div style={{display:"flex", flexDirection:"row", alignItems:"center", width:"70%", justifyContent:"space-between", marginBottom:"5%"}}>
-                    <a href="https://spotifyjazzed-v2.onrender.com//login"><button type="button" className="Login">Login</button></a>
+                    <a href="https://spotifyjazzed-v2.onrender.com/login"><button type="button" className="Login">Login</button></a>
                 </div>
             </div>
         </div>
